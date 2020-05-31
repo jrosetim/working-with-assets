e36be70e99ca5bb7c36dedcc95d96440629c72f9
Julio Cesar Rosetim <jrosetim@gmail.com>
2020-05-31 11:35:30 -0300

    commit

--- src/components/actions/actions.story.js
+++ src/components/actions/actions.story.js
@@ -1,7 +1,13 @@
 import React from 'react'
-import {storiesOf} from '@kadira/storybook'
+import {storiesOf, action} from '@kadira/storybook'
+import Actions from './index'
 
-storiesOf('Div', module)
-  .add('first story', () => (
-    <div>Hello</div>
+const stories = storiesOf('Actions', module)
+
+
+stories.add('first story', () => (
+    <Actions>
+      getRepos = { action('Get Repos') },
+      getStarred = { action('Get Starred')}
+    </Actions>
   ))
---
+++ storybook/webpack.config.js
@@ -0,0 +1,7 @@
+const webpackConfig = require('@kadira/storybook/dist/server/config/defaults/webpack.config.js');
+
+module.exports = function (config, env){
+  const newConfig = webpackConfig(config, env);
+
+  return newConfig;
+}
